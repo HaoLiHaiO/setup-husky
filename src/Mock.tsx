e7c96e5f585a923f475def3747a0a8a50de0cc89
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-24 19:05:35 +0300

    fix import

--- src/Mock.tsx
+++ src/Mock.tsx
@@ -1,19 +1,19 @@
-import logo from '../public/logo.svg';
+import logo from '../src/logo.svg';
 
 export const MockComponent = () => (
     <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <p>
-        Edit <code>src/App.tsx</code> and save to reload.
+        This is just a mock component.
       </p>
       <a
         className="App-link"
-        href="https://reactjs.org"
+        href="https://typicode.github.io/husky/#/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        Learn React
+        Set up husky
       </a>
     </header>
   </div>
